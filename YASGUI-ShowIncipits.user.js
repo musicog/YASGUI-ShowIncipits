6167648fd659d6176d455f593318e774c94216fb
Stefan@AWG <stefan.muennich@unibas.ch>
2019-05-23 15:05:41 +0200

    enhance(js): add handling of musicxml input in lum sparql endpoint

--- YASGUI-ShowIncipits.user.js
+++ YASGUI-ShowIncipits.user.js
@@ -7,19 +7,33 @@
 // @run-at document-idle
 // @include  http://yasgui.org/*
 // @include  https://yasgui.org/*
+// @include  http://linkeddata.uni-muenster.de:7200/sparql
 // @require  http://www.verovio.org/javascript/latest/verovio-toolkit.js
 // ==/UserScript==
 
+
+// check if we are on lum sparql endpoint
+var isLum = (window.location.href === "http://linkeddata.uni-muenster.de:7200/sparql");
+
+// if so, input format is musicxml, otherwise Plaine&Easie
+var inputFormat = isLum ? 'xml' : 'pae';
+
+// set verovio options
 const vrvOptions = {
-  inputFormat: 'pae',
+  inputFormat: inputFormat,
   pageHeight: 2970,
   pageWidth: 1400,
   spacingStaff: 0,
-  border: 20,
+  pageMarginBottom: 20,
+  pageMarginLeft: 20,
+  pageMarginRight: 20,
+  pageMarginTop: 20,
   adjustPageHeight: 1,
   scale: 45,
-  xmlIdSeed:1
+  xmlIdSeed: 1
 };
+
+// create verovio toolkit instance
 var vrvToolkit = new verovio.toolkit();
 tick();
 
@@ -42,17 +56,19 @@ function tick() {
       rows.forEach( (row, index) => { 
         if(index === 0) { return } // skip header row
         // grab the incipit column's contents
-         const incipitCell = row.children.item(incipitColumnIndex+1);
+        const incipitCell = row.children.item(incipitColumnIndex+1);
         if(incipitCell.firstChild.nodeName === "svg") { 
           // we've already converted this one, so skip ahead
           return 
         }
+
+        // define input data depending on the input format
+        var vrvData = row.children.item(incipitColumnIndex+1).innerText;
+        if(!isLum) {
+          vrvData = "@clef:G-2\n\@keysig:\n\@timesig:\n\@data:" + vrvData + "\n" ;
+        }
         // ask Verovio to make us an SVG
-        incipitCell.innerHTML = vrvToolkit.renderData(
-          "@clef:G-2\n\@keysig:\n\@timesig:\n\@data:" + 
-          row.children.item(incipitColumnIndex+1).innerText + "\n", 
-          vrvOptions
-        );
+        incipitCell.innerHTML = vrvToolkit.renderData(vrvData, vrvOptions);
 
         // extract the MEI data from Verovio Toolkit and append it to the row
         const meiData = vrvToolkit.getMEI();
